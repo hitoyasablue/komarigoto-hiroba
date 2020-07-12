88726699f67f362dd399a15015ae72235ce3d4e4
Yusaku <hitoyasablue@gmail.com>
2020-07-12 23:43:32 +0900

    投稿機能と投稿一覧機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -10,5 +12,17 @@ class PostsController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save!
+    redirect_to posts_url, notice: "投稿しました"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
