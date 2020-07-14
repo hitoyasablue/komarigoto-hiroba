b1ec0a24dc1fa91ebd1f88a2e9b82f61a30340ba
Yusaku <hitoyasablue@gmail.com>
2020-07-14 22:25:33 +0900

    ユーザーと投稿の紐付け

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  include SessionsHelper
   before_action :logged_in_user, only:[:index, :edit, :update, :destroy]
   before_action :correct_user, only:[:edit, :update]
   before_action :admin_user, only: :destroy
@@ -9,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = current_user.posts.order(created_at: :desc)
   end
 
   def new
