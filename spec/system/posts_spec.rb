6ebc6d67ed3c81970439bf019a81ac69eb4988df
Yusaku <hitoyasablue@gmail.com>
2020-07-17 18:49:41 +0900

    rubocopに従ってコードを整形

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe '投稿のシステムテスト'
-  describe '一覧表示機能' do
+describe '一覧表示機能' do
+  before do
+    user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+    FactoryBot.create(:post, content: 'おめシスはいいぞ',user: user_a)
+  end
+  context 'ユーザーAがログインしているとき' do
     before do
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      FactoryBot.create(:post, content: 'おめシスはいいぞ',user: user_a)
+      visit login_path
+      fill_in 'session[email]', with: 'a@example.com'
+      fill_in 'session[password]', with: 'password'
+      click_button 'ログイン'
     end
-    context 'ユーザーAがログインしているとき' do
-      before do
-        visit login_path
-        fill_in 'session[email]', with: 'a@example.com'
-        fill_in 'session[password]', with: 'password'
-        click_button 'ログイン'
-      end
 
-      it 'ユーザーAが投稿した困りごとが表示される' do
-        expect(page).to have_content 'おめシスはいいぞ'
-      end
+    it 'ユーザーAが投稿した困りごとが表示される' do
+      expect(page).to have_content 'おめシスはいいぞ'
     end
   end
+end
