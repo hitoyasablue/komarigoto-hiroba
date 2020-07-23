1c4e3d2a029add99c3d742fd772becf439db1e19
Yusaku <hitoyasablue@gmail.com>
2020-07-23 21:12:19 +0900

    進捗, いいね, ユーザー, 通知機能のシステムテストを作成

---
+++ spec/system/likes_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe '「えらい」のシステムテスト' do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let!(:post_a) { FactoryBot.create(:post, content: 'Aの投稿', user: user_a) }
+
+  before do
+    visit login_path
+    fill_in 'session[email]', with: user_a.email
+    fill_in 'session[password]', with: user_a.password
+    click_button 'ログイン'
+  end
+
+  describe '「えらい」表示機能' do
+    before do
+      visit post_path(post_a)
+    end
+
+    context 'すでに投稿に「えらい」をしている場合', js: true do
+      before do
+        click_button 'えらい'
+      end
+
+      it '「えらい済」ボタンが表示されている' do
+        expect(page).to have_css '#unlike-button'
+      end
+    end
+
+    context 'まだ投稿に「えらい」をしていない場合' do
+      it '「えらい」ボタンが表示されている' do
+        expect(page).to have_css '#like-button'
+      end
+    end
+  end
+end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe 'ユーザーのシステムテスト', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:post_a) { FactoryBot.create(:post, content: 'Aの投稿', user: user_a) }
+
+  before do
+    visit login_path
+    fill_in 'session[email]', with: login_user.email
+    fill_in 'session[password]', with: login_user.password
+    click_button 'ログイン'
+  end
+
+  describe 'ユーザー詳細ページ表示機能' do
+    # before do
+    #   visit user_path(user_a)
+    # end
+
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      it 'ユーザーAが作成した投稿が表示される' do
+        expect(page).to have_content 'Aの投稿'
+      end
+    end
+
+    context 'ユーザーBがログインしているとき' do
+      let(:login_user) { user_b }
+
+      it 'ユーザーAが作成した投稿が表示されない' do
+        expect(page).to have_no_content 'Aの投稿'
+      end
+    end
+  end
+
+  describe 'ユーザー編集機能' do
+    let(:login_user) { user_a }
+
+    before do
+      visit edit_user_path(user_a)
+    end
+
+    it '編集処理' do
+      fill_in 'user_name', with: 'ユーザーC'
+      fill_in 'user_password', with: 'password'
+      fill_in 'user_password_confirmation', with: 'password'
+      click_button '変更'
+      expect(page).to have_selector '.alert-success', text: 'プロフィールを更新しました'
+    end
+  end
+end
