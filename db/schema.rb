fc665050157d54b4139f507c520c9162d9f0dfcf
Yusaku <hitoyasablue@gmail.com>
2020-08-31 15:27:05 +0900

    テストコードの修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,136 +12,136 @@
 
 ActiveRecord::Schema.define(version: 2020_08_04_154109) do
 
-  create_table 'erais', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'progress_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['progress_id'], name: 'index_erais_on_progress_id'
-    t.index ['user_id', 'progress_id'], name: 'index_erais_on_user_id_and_progress_id', unique: true
-    t.index ['user_id'], name: 'index_erais_on_user_id'
+  create_table "erais", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "progress_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["progress_id"], name: "index_erais_on_progress_id"
+    t.index ["user_id", "progress_id"], name: "index_erais_on_user_id_and_progress_id", unique: true
+    t.index ["user_id"], name: "index_erais_on_user_id"
   end
 
-  create_table 'likes', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'post_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['post_id'], name: 'index_likes_on_post_id'
-    t.index ['user_id', 'post_id'], name: 'index_likes_on_user_id_and_post_id', unique: true
-    t.index ['user_id'], name: 'index_likes_on_user_id'
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'notifications', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'visitor_id', null: false
-    t.integer 'visited_id', null: false
-    t.integer 'post_id'
-    t.integer 'progress_id'
-    t.string 'action', default: '', null: false
-    t.boolean 'checked', default: false, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['post_id'], name: 'index_notifications_on_post_id'
-    t.index ['progress_id'], name: 'index_notifications_on_progress_id'
-    t.index ['visited_id'], name: 'index_notifications_on_visited_id'
-    t.index ['visitor_id'], name: 'index_notifications_on_visitor_id'
+  create_table "notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "visitor_id", null: false
+    t.integer "visited_id", null: false
+    t.integer "post_id"
+    t.integer "progress_id"
+    t.string "action", default: "", null: false
+    t.boolean "checked", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_notifications_on_post_id"
+    t.index ["progress_id"], name: "index_notifications_on_progress_id"
+    t.index ["visited_id"], name: "index_notifications_on_visited_id"
+    t.index ["visitor_id"], name: "index_notifications_on_visitor_id"
   end
 
-  create_table 'ouen2s', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'progress_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['progress_id'], name: 'index_ouen2s_on_progress_id'
-    t.index ['user_id', 'progress_id'], name: 'index_ouen2s_on_user_id_and_progress_id', unique: true
-    t.index ['user_id'], name: 'index_ouen2s_on_user_id'
+  create_table "ouen2s", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "progress_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["progress_id"], name: "index_ouen2s_on_progress_id"
+    t.index ["user_id", "progress_id"], name: "index_ouen2s_on_user_id_and_progress_id", unique: true
+    t.index ["user_id"], name: "index_ouen2s_on_user_id"
   end
 
-  create_table 'ouens', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'post_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['post_id'], name: 'index_ouens_on_post_id'
-    t.index ['user_id', 'post_id'], name: 'index_ouens_on_user_id_and_post_id', unique: true
-    t.index ['user_id'], name: 'index_ouens_on_user_id'
+  create_table "ouens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_ouens_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_ouens_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_ouens_on_user_id"
   end
 
-  create_table 'posts', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.text 'content'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'user_id', null: false
-    t.text 'optional_content'
-    t.text 'optional_content_2'
-    t.text 'optional_content_3'
-    t.text 'optional_content_4'
-    t.index ['user_id'], name: 'index_posts_on_user_id'
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.text "optional_content"
+    t.text "optional_content_2"
+    t.text "optional_content_3"
+    t.text "optional_content_4"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table 'progresses', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.text 'content'
-    t.bigint 'post_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.text 'optional_content'
-    t.text 'optional_content_2'
-    t.text 'optional_content_3'
-    t.text 'optional_content_4'
-    t.text 'content_2'
-    t.index ['post_id'], name: 'index_progresses_on_post_id'
+  create_table "progresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "optional_content"
+    t.text "optional_content_2"
+    t.text "optional_content_3"
+    t.text "optional_content_4"
+    t.text "content_2"
+    t.index ["post_id"], name: "index_progresses_on_post_id"
   end
 
-  create_table 'sounandas', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'progress_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['progress_id'], name: 'index_sounandas_on_progress_id'
-    t.index ['user_id', 'progress_id'], name: 'index_sounandas_on_user_id_and_progress_id', unique: true
-    t.index ['user_id'], name: 'index_sounandas_on_user_id'
+  create_table "sounandas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "progress_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["progress_id"], name: "index_sounandas_on_progress_id"
+    t.index ["user_id", "progress_id"], name: "index_sounandas_on_user_id_and_progress_id", unique: true
+    t.index ["user_id"], name: "index_sounandas_on_user_id"
   end
 
-  create_table 'teinei2s', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'progress_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['progress_id'], name: 'index_teinei2s_on_progress_id'
-    t.index ['user_id', 'progress_id'], name: 'index_teinei2s_on_user_id_and_progress_id', unique: true
-    t.index ['user_id'], name: 'index_teinei2s_on_user_id'
+  create_table "teinei2s", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "progress_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["progress_id"], name: "index_teinei2s_on_progress_id"
+    t.index ["user_id", "progress_id"], name: "index_teinei2s_on_user_id_and_progress_id", unique: true
+    t.index ["user_id"], name: "index_teinei2s_on_user_id"
   end
 
-  create_table 'teineis', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'post_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['post_id'], name: 'index_teineis_on_post_id'
-    t.index ['user_id', 'post_id'], name: 'index_teineis_on_user_id_and_post_id', unique: true
-    t.index ['user_id'], name: 'index_teineis_on_user_id'
+  create_table "teineis", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_teineis_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_teineis_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_teineis_on_user_id"
   end
 
-  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'password_digest'
-    t.boolean 'admin', default: false
-    t.string 'image'
-    t.index ['email'], name: 'index_users_on_email', unique: true
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.string "image"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table 'wakarus', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'post_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['post_id'], name: 'index_wakarus_on_post_id'
-    t.index ['user_id', 'post_id'], name: 'index_wakarus_on_user_id_and_post_id', unique: true
-    t.index ['user_id'], name: 'index_wakarus_on_user_id'
+  create_table "wakarus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_wakarus_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_wakarus_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_wakarus_on_user_id"
   end
 
-  add_foreign_key 'progresses', 'posts'
+  add_foreign_key "progresses", "posts"
 end
